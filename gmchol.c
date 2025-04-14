e77fb790e45feb76fd71993099320a1510b5891b
wyattgill9 <wyattgill2009@gmail.com>
2025-04-13 18:22:12 -0700

    Better C

--- gmchol.c
+++ gmchol.c
@@ -4,91 +4,69 @@
 #include <float.h>
 
 void gmchol(double **A, int n, double **R) {
-    double **E = (double **)malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        E[i] = (double *)calloc(n, sizeof(double));
-    }
-    
-    double norm_A = 0.0;
+    double norm_A = 0.0, gamm = 0.0;
+
     for (int j = 0; j < n; j++) {
         double col_sum = 0.0;
         for (int i = 0; i < n; i++) {
-            col_sum += fabs(A[i][j]);
+            double val = fabs(A[i][j]);
+            col_sum += val;
+            if (i == j && val > gamm) gamm = val;
         }
-        if (col_sum > norm_A) {
-            norm_A = col_sum;
-        }
-    }
-
-    double gamm = 0.0;
-    for (int i = 0; i < n; i++) {
-        if (fabs(A[i][i]) > gamm) {
-            gamm = fabs(A[i][i]);
-        }
+        if (col_sum > norm_A) norm_A = col_sum;
     }
 
     double delta = fmax(DBL_EPSILON * norm_A, DBL_EPSILON);
-    
+
     for (int j = 0; j < n; j++) {
         double theta_j = 0.0;
 
-        for (int i = 0; i < n; i++) {
-            double sum_val = 0.0;
+        for (int i = 0; i <= j; i++) {
+            double sum = 0.0;
             for (int k = 0; k < i; k++) {
-                sum_val += R[k][i] * R[k][j];
-            }
-            R[i][j] = (A[i][j] - sum_val) / R[i][i];
-
-            if ((A[i][j] - sum_val) > theta_j) {
-                theta_j = A[i][j] - sum_val;
+                sum += R[k][i] * R[k][j];
             }
 
-            if (i > j) {
-                R[i][j] = 0.0;
-            }
-        }
-
-        double sum_val = 0.0;
-        for (int k = 0; k < j; k++) {
-            sum_val += R[k][j] * R[k][j];
-        }
-        double phi_j = A[j][j] - sum_val;
-
-        double xi_j;
-        if ((j + 1) < n) {
-            xi_j = 0.0;
-            for (int i = j + 1; i < n; i++) {
-                if (fabs(A[i][j]) > xi_j) {
-                    xi_j = fabs(A[i][j]);
+            if (i < j) {
+                R[i][j] = (A[i][j] - sum) / R[i][i];
+                double residual = A[i][j] - sum;
+                if (fabs(residual) > theta_j) theta_j = fabs(residual);
+            } else {
+                double phi_j = A[j][j];
+                for (int k = 0; k < j; k++) {
+                    phi_j -= R[k][j] * R[k][j];
                 }
+
+                double xi_j = 0.0;
+                for (int t = j + 1; t < n; t++) {
+                    double val = fabs(A[t][j]);
+                    if (val > xi_j) xi_j = val;
+                }
+
+                double beta_j = sqrt(fmax(gamm, fmax(xi_j / n, DBL_EPSILON)));
+                double threshold = fmax(fabs(phi_j), (theta_j * theta_j) / (beta_j * beta_j));
+
+                double correction = 0.0;
+                if (delta >= threshold) {
+                    correction = delta - phi_j;
+                } else if (fabs(phi_j) >= fmax((delta * delta) / (beta_j * beta_j), delta)) {
+                    correction = fabs(phi_j) - phi_j;
+                } else {
+                    correction = ((theta_j * theta_j) / (beta_j * beta_j)) - phi_j;
+                }
+
+                R[j][j] = sqrt(phi_j + correction);
             }
-        } else {
-            xi_j = fabs(A[n - 1][j]);
         }
 
-        double beta_j = sqrt(fmax(gamm, fmax(xi_j / n, DBL_EPSILON)));
-
-        if (delta >= fmax(fabs(phi_j), (theta_j * theta_j) / (beta_j * beta_j))) {
-            E[j][j] = delta - phi_j;
-        } else if (fabs(phi_j) >= fmax((delta * delta) / (beta_j * beta_j), delta)) {
-            E[j][j] = fabs(phi_j) - phi_j;
-        } else if ((theta_j * theta_j) / (beta_j * beta_j) >= fmax(delta, fabs(phi_j))) {
-            E[j][j] = ((theta_j * theta_j) / (beta_j * beta_j)) - phi_j;
+        for (int i = j + 1; i < n; i++) {
+            R[i][j] = 0.0;
         }
-
-        R[j][j] = sqrt(A[j][j] - sum_val + E[j][j]);
     }
-
-    // Free allocated memory for E
-    for (int i = 0; i < n; i++) {
-        free(E[i]);
-    }
-    free(E);
 }
 
-// Example
 int main() {
-    int n = 3; // Matrix size
+    int n = 3;
     double **A = (double **)malloc(n * sizeof(double *));
     double **R = (double **)malloc(n * sizeof(double *));
     for (int i = 0; i < n; i++) {
